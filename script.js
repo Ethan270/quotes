0ae28312499a08778acb39e106eddc803209a90c
[Ethan Gonzalez] <[ethang27000@gmail.com]>
2024-09-22 23:51:42 -0500

    FirstCommit

---
+++ script.js
@@ -0,0 +1,19 @@
+var QuotesOfTheDay = ["If you only do what you can do, you will never be more than you are.",
+     "Nothing will work unless you do.", 
+     "Don't let someone else's opinion of you become your reality.",
+      "If life gives you lemons, make lemonade.", 
+      "Life begins at the end of your comfort zone."
+    ];
+    alert(QuotesOfTheDay[0]);
+    QuotesOfTheDay[0] = "If you only do what you can do, you will never be more than you are.";
+    QuotesOfTheDay[QuotesOfTheDay.length-1]
+
+    var FavoriteURLs= ["https://www.youtube.com/",
+        "https://open.spotify.com/",
+        "https://www.amazon.com/",
+    ];
+
+    
+
+
+    
